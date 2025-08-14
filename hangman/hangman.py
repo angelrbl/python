b77c128c10a7452d0696e540b188aad7df5a405f
angelrb <angelramibla@gmail.com>
2025-08-14 11:35:00 +0200

    Implement basic game logic for Hangman
    Added the initial implementation of the Hangman game logic, including random word selection and letter guessing. The main function now starts the game after language and difficulty selection.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 file_name = 'hangman/words.json'
 words = {}
@@ -35,13 +36,27 @@ def choose_dif(words):
             print("Invalid input, please try again: ")
             choose_dif(words)
 
-def game_logic():
-    ...
+def game_logic(words):
+    guess = []
+    word = list(words[random.randint(0, len(words)-1)])
+    for char in word:
+        guess.append('_')
+        
+    print(guess)
+    print(char for char in guess)
+    while word != guess:
+        guesschar = input("Type one letter to guess: ")
+        for char in word:
+            if char == guesschar:
+                guess[word.index(char)] = guesschar
+        print(guess)
+        print(char for char in guess)
+    
 
 def main():
     valid_words = choose_lang(words)
     valid_words = choose_dif(valid_words)
-    print(valid_words)
+    game_logic(valid_words)
 
 if __name__ == "__main__":
     words = load_values(file_name)
