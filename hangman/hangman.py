75024d351d35329abfa8cb87f6fba9a7f2689e4e
angelrb <angelramibla@gmail.com>
2025-08-14 17:39:05 +0200

    Refactor hangman game flow and add settings menu
    Reorganized the main game loop to include a menu for playing, changing language and difficulty, or exiting. Improved input validation, added tracking for used letters and strikes, and refactored difficulty selection to set max strikes. Enhanced user feedback and error handling for missing word lists.

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,16 +1,20 @@
 import json
 import random
+from sys import exit
 
 file_name = 'hangman/words.json'
 words = {}
 valid_words = []
+max_strikes: int = 0
 
 def load_values(file_name):
     try:
         with open(file_name, 'r') as f:
             return json.load(f)
     except:
-        print("Cannot find a valid word list")
+        print("Cannot find a valid word list, please provide one.")
+        exit(0)
+
 
 def choose_lang(words):
     lang = input("\nWhat language are you playing in? (en/es): ")
@@ -20,44 +24,84 @@ def choose_lang(words):
         case 'es':
             return [word for word in words.keys() if words.get(word) == 'es']
         case _:
-            print("Invalid input, please try again: ")
-            choose_lang(words)
+            print("Invalid input.")
+            main()
 
 def choose_dif(words):
-    dif = input("\nHow difficult do you want the game to be? (easy/hard/all): ")
+    dif = input("How difficult do you want the game to be? (easy/normal/hard): ")
     match dif:
         case 'easy':
-            return [word for word in words if len(word) <= 4]
+            return [word for word in words if len(word) <= 4], 7
+        case 'normal':
+            return words, 6
         case 'hard':
-            return [word for word in words if len(word) > 4]
-        case 'all':
-            return words
+            return [word for word in words if len(word) > 4], 5
         case _:
-            print("Invalid input, please try again: ")
-            choose_dif(words)
+            print("Invalid input.")
+            main()
 
-def game_logic(words):
+def game_logic(words, max_strikes):
     guess = []
+    used_chars = []
     word = list(words[random.randint(0, len(words)-1)])
+    strikes = 0
+
     for char in word:
         guess.append('_')
-        
-    print(guess)
-    print(char for char in guess)
-    while word != guess:
+
+    print("\nYOUR WORD IS:")
+    print(" ".join(guess), end='\n\n')
+
+    while word != guess or strikes <= max_strikes:
+        correct = False
         guesschar = input("Type one letter to guess: ")
-        for char in word:
+        used_chars.append(guesschar)
+        for i, char in enumerate(word):
             if char == guesschar:
-                guess[word.index(char)] = guesschar
-        print(guess)
-        print(char for char in guess)
+                guess[i] = guesschar
+                correct = True
+        if correct == False:
+            strikes += 1
+        if strikes >= max_strikes:
+            print(f"Sorry, but you reach the limit of {max_strikes} strikes, you lost.")
+            strikes += 1
+            main()
+        print(f'{" ".join(guess)}   Strikes: {strikes}/{max_strikes}  Letters tried: {" ".join(used_chars)}\n')
     
+    print(f"CONGRATULATIONS, YOU WON!! THE WORD WAS {"".join(word).upper()}. GOING BACK...")
+    main()
+
+def close():
+    exit("Alright, see you soon!")
+
+
+def choose_settings():
+    global valid_words
+    global max_strikes
+    valid_words = choose_lang(words)
+    valid_words, max_strikes = choose_dif(valid_words)
+    main()
 
 def main():
-    valid_words = choose_lang(words)
-    valid_words = choose_dif(valid_words)
-    game_logic(valid_words)
+    
+    print("\nHANGMAN:")
+    print("[1] Play")
+    print("[2] Change language & difficulty")
+    print("[0] Exit")
+
+    action = input("What do you want to do?: ")
+
+    match action:
+        case "1":
+            game_logic(valid_words, max_strikes)
+        case "2":
+            choose_settings()
+        case "0":
+            close()
+        case _:
+            print("Invalid input, try again")
+            main()
 
 if __name__ == "__main__":
     words = load_values(file_name)
-    main()
+    choose_settings()
