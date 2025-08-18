57b21504d7dda247b32c010bb7cbb0ddddb3a2c0
angelrb <angelramibla@gmail.com>
2025-08-18 11:17:05 +0200

    Improve lyrics formatting and input handling
    Added functions to format lyrics by inserting newlines around brackets and between lowercase-uppercase transitions. Updated user input to replace spaces with hyphens for URL compatibility.

--- lyrics.py
+++ lyrics.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 author: str = ""
 song_name: str = ""
@@ -9,10 +10,26 @@ def scrape_lyrics():
 
     response = requests.get(url)
     doc = BeautifulSoup(response.text, "html.parser")
-    return doc.find("div", {"class": "Lyrics__Container-sc-a49d8432-1 fBKwZw"}).text.strip
+    lyrics_containers = doc.find("div", {"class": "Lyrics__Container-sc-a49d8432-1 fBKwZw"}).text
+
+    lyrics = format_lyrics_1(lyrics_containers)
+    lyrics = format_lyrics_2(lyrics)
+    lyrics = add_new_line(lyrics)
+
+    return lyrics
+
+
+def format_lyrics_1(str):
+    return re.sub(r"([\[])", r"\n\1", str)
+
+def format_lyrics_2(str):
+    return re.sub(r"([\]])", r"\1\n", str)
+
+def add_new_line(str):
+    return re.sub(r"([a-z])([A-Z])", r"\1\n\2", str)
 
 if __name__ == "__main__":
-    author = input("Please provide an author name: ")
-    song_name = input(f"Please provide a song written by {author}: ")
+    author = input("Please provide an author name: ").replace(" ", "-")
+    song_name = input(f"Please provide a song written by {author}: ").replace(" ", "-")
 
     print(scrape_lyrics())
